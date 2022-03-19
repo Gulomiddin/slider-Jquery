a75bd218e94e87b01782a64122fcdfe817bbe238
gulomiddin <sergokomola@gmail.com>
2022-03-19 17:03:08 +0500

    load

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('#autoWidth').lightSlider({
+        autoWidth: true,
+        loop: true,
+        autoPlay: true,
+        onSliderLoad: function () {
+            $('#autoWidth').removeClass('cS-hidden');
+        }
+    });
+
+
+});
